ea6d0c0afc7177a246f66804e99559810b37700f
Enow Mbi <benowmbi@yahoo.com>
2019-08-29 15:46:55 +0100

    Modify assignments specs for models

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,62 @@
+
+
+
+
+
+
+
+
+
+# TODO: maybe add one for :has_and_belongs_to_many?
+# TODO: Implement insertions for existing files... why??
+
 require 'rails_helper'
 
 RSpec.describe Assignment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory" do
+    assignment = FactoryGirl.build(:assignment)
+    expect(assignment).to be_valid
+  end
+
+  describe "Validators" do
+
+  	
+  		
+  			it "should ensure the presence of tag" do
+  				assignment = FactoryGirl.build(:assignment, tag: nil)
+  				expect(assignment).not_to be_valid
+  				expect(assignment.errors[:tag]).to be_present
+  			end
+  		
+  	
+
+  	
+  end
+
+  
+
+	  describe "Associations" do
+	  	
+	  		it "should belong to a tag" do
+	  			tag = FactoryGirl.create(:tag)
+	  			assignment = FactoryGirl.build(:assignment, tag: tag)
+	  			expect(assignment.tag).to eq tag
+	  		end
+	  	
+	  		it "should belong to a testimony" do
+	  			testimony = FactoryGirl.create(:testimony)
+	  			assignment = FactoryGirl.build(:assignment, testimony: testimony)
+	  			expect(assignment.testimony).to eq testimony
+	  		end
+	  	
+
+	  	
+	  end
+	
+
+		
+
+  describe "Behavior" do
+  	pending "add some examples to #{__FILE__} for behaviours or delete the 'Behaviour' test there."
+  end
 end
