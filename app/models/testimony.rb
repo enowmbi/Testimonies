2bf04b1719d2d06ffa46e1f92518c42f2d06ba46
Enow Mbi <benowmbi@yahoo.com>
2019-08-29 16:49:13 +0100

    Modify testimony -add dependent destroy for assignments

--- app/models/testimony.rb
+++ app/models/testimony.rb
@@ -1,6 +1,6 @@
 class Testimony < ApplicationRecord
   belongs_to :user
-  has_many :assignments
+  has_many :assignments, dependent: :destroy
   has_many :tags, through: :assignments
 
   validates :title, presence: true, uniqueness: true
