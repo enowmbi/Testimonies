d74edc23a2a070e9338ecdc24c569abcf39f2dba
Enow Mbi <benowmbi@yahoo.com>
2020-04-07 06:42:32 +0100

    Modify pacs/application.js --add require for @rails/ujs, turbolinks,@rails/activestorage,channels, also import jquery,import bootstrap, and popper.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,8 +14,13 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-console.log('Hello World from Webpacker')
-import 'jquery'
-import 'bootstrap'
+import "jquery";
+import "bootstrap";
+import "popper.js";
+console.log('Hello World from Webpacker');
 
